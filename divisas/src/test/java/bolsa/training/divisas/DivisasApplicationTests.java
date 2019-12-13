39d0a36c9ed197d5b929150f9a23acefeaeeaca6
CARLOSPONC3 <CARLOSPONC3@users.noreply.github.com>
2019-12-13 16:56:30 -0600

    upload
    First commit, including eureka server and divisas client

---
+++ divisas/src/test/java/bolsa/training/divisas/DivisasApplicationTests.java
@@ -0,0 +1,16 @@
+package bolsa.training.divisas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DivisasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
